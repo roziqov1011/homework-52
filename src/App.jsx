fe7c508babeac4d8379de8a7d52d7f059a255421
roziqov1011 <roziqov1011@gmail.com>
2021-11-09 08:34:36 +0500

    git init

---
+++ src/App.jsx
@@ -0,0 +1,85 @@
+import './App.scss';
+import background from './img/background.jpg'
+import preview from './img/preview.jpg'
+import arrowback from './img/arrow-back.svg'
+import play from './img/play.svg'
+import arrowforward from './img/arrow-forward.svg'
+import { useState } from 'react';
+
+import content from './assets/content';
+
+function App() {
+
+  const [lang , setLang] = useState("uz")
+  return (
+    <>
+    
+
+
+
+    {/* ================= */}
+
+  <div className="wrapper">
+    <div className="section">
+      <div className="left">
+        <img src={background} alt="background" class="img"/>
+        <div className="back">
+          <a href="/">
+            <img src={arrowback} alt="back"/>
+          <p>{content[lang].back}</p>
+          </a>
+        </div>
+      </div>
+      <div className="right">
+        <div className="nav">
+          
+          <ul>
+            <li><a href="/">{content[lang].link.state1}</a></li>
+            <li><a href="/" class="aktiv">{content[lang].link.state2}</a></li>
+            <li><a href="/">{content[lang].link.state3}</a></li>
+            <li>
+            <select 
+                  defaultValue={lang}
+                  onChange={
+                    (evt)=>{
+                      setLang(evt.target.value)
+                    }
+                  }>
+                  <option value="uz">UZB</option>
+                  <option value="en">EN</option>
+                  <option value="ru">RU</option>
+          </select>
+            </li>
+          </ul>
+        </div>
+        <div className="main">
+          <div className="title">
+            <h1>{content[lang].title}</h1>
+            <span></span>
+          </div>
+          <div className="subtitle">
+            <h4>{content[lang].subTitle}</h4>
+          </div>
+          <div className="block">
+            <div className="block-left">
+              <img src={preview} alt="" width="200"/>
+              <img src={play} alt="play" class="play"/>
+              <div className="all-foto">
+                <a href="/">{content[lang].videoText}</a>
+                <img src={arrowforward} alt=""/>
+              </div>
+            </div>
+            <div className="block-right"> 
+              <p>{content[lang].text}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    {/* =============== */}
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/content.js
@@ -0,0 +1,40 @@
+const content = {
+     uz:{
+            title:"Pastel shahriga tashrif",
+            link:{
+                state1:"GOLLANDIYA",
+                state2:"ISPANIYA",
+                state3:"FRANSIYA",
+            },
+            back:"ORQAGA",
+            subTitle:"Bu Jerri Xenderson, turizm sohasi agenti",
+            text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam voluptate magnam magni nihil harum atque quas id saepe eveniet. Corporis atque maiores quis!",
+            videoText:"barcha fotosuratlarni ko'rish"
+        },
+        en:{
+            title:"Visiting the pastelle city",
+            link:{
+                state1:"NETHERLANDS",
+                state2:"SPAIN",
+                state3:"FRANCE",
+            },
+            back:"BACK",
+            subTitle:"Bu Jerry Henderson, in Tourism",
+            text:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam voluptate magnam magni nihil harum atque quas id saepe eveniet. Corporis atque maiores quis!",
+            videoText:"view all photos"
+        },
+        ru:{
+            title:"Посещение пастельного города",
+            link:{
+                state1:"НИДЕРЛАНДЫ",
+                state2:"ИСПАНИЯ",
+                state3:"ФРАНЦИЯ",
+            },
+            back:"НАЗАД",
+            subTitle:"Бу Джерри Хендерсон, в туризме",
+            text:"Далеко-далеко за словесными горами в стране гласных и согласных, живут рыбные тексты. Путь, маленький переписали живет там грамматики подзаголовок они ",
+            videoText:"просмотреть все фото"
+        }
+}
+
+export default content;
